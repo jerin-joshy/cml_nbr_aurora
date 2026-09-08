b52e6dc669fc137d96806c47f61a19545cd821db
Jerin Joshy <jerinjoshy1@gmail.com>
2026-09-08 20:12:16 +0530

    added forced dymanics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { getOrgSettings } from "@/lib/org-settings";
 import { FeastShell } from "@/components/feast/feast-shared";
 import { FeastLanding } from "@/components/feast/feast-landing";
 
+export const dynamic = 'force-dynamic';
 export default async function Home() {
   const org = await getOrgSettings();
   return (
